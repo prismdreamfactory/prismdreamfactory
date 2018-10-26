e3e40da1fd373dfd5d022391e53168c7ff597189
Eric K Wu <ekuan.wu@gmail.com>
2018-10-26 11:26:02 -0700

    Add favicon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.scss";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import Home from "./components/Home";
 import Work from "./components/Work";
 import About from "./components/About";
@@ -8,8 +8,22 @@ import Contact from "./components/Contact";
 import bg from "./assets/images/bg.jpg";
 import logo from "./assets/images/logo.png";
 
+import { isBrowser, isMobile } from "react-device-detect";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOpen: false };
+  }
+
+  // Toggle mobile navigation
+  toggleMenu = () => {
+    this.setState(state => ({ isOpen: !state.isOpen }));
+  };
+
   render() {
+    const { isOpen } = this.state;
+
     return (
       <Router>
         <div className="app">
@@ -21,18 +35,47 @@ class App extends Component {
             alt="Prism Dream Factory Logo"
           />
 
-          <Link to="/" className="app__link mod--top-left">
-            Prism Dream Factory
-          </Link>
-          <Link to="/about" className="app__link mod--top-right">
-            About
-          </Link>
-          <Link to="/work" className="app__link mod--bottom-left">
-            Work
-          </Link>
-          <Link to="/contact" className="app__link mod--bottom-right">
-            Contact
-          </Link>
+          <nav className="nav--desktop">
+            <NavLink to="/" className="app__link mod--top-left">
+              Prism Dream Factory
+            </NavLink>
+            <NavLink to="/about" className="app__link mod--top-right">
+              About
+            </NavLink>
+            <NavLink to="/work" className="app__link mod--bottom-left">
+              Work
+            </NavLink>
+            <NavLink to="/contact" className="app__link mod--bottom-right">
+              Contact
+            </NavLink>
+          </nav>
+
+          <nav className={isOpen ? "nav--mobile mod--open" : "nav--mobile"}>
+            <NavLink to="/" className="app__link">
+              Home
+            </NavLink>
+            <NavLink to="/about" className="app__link">
+              About
+            </NavLink>
+            <NavLink to="/work" className="app__link">
+              Work
+            </NavLink>
+            <NavLink to="/contact" className="app__link">
+              Contact
+            </NavLink>
+          </nav>
+
+          <div class="nav__toggle">
+            <div
+              className={isOpen ? "nav__icon mod--open" : "nav__icon"}
+              onClick={this.toggleMenu}
+            >
+              <span />
+              <span />
+              <span />
+              <span />
+            </div>
+          </div>
 
           <Route path="/" exact component={Home} />
           <Route path="/work" exact component={Work} />
